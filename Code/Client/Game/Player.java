9e85376a29f7ce5790f79683404ea57c566f84b7
PhaPhePha <phanguyen462@gmail.com>
2026-04-11 21:46:07 +0700

    thêm code GameManager.java và thêm check boundary & getSize(cho UI xài) cho Board.java

--- Code/Client/Game/Player.java
+++ Code/Client/Game/Player.java
@@ -3,7 +3,7 @@ package Code.Client.Game;
 public class Player {
     private int playerId;
     private String playerName;
-    private int symbol;
+    private int symbol; //1 hoac 2 (x or o)
     public Player(int playerId, String playerName, int symbol) {
         this.playerId = playerId;
         this.playerName = playerName;
